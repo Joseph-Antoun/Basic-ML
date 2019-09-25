Test on the data for wine and fixed errors in logistic regression and LDA more testing should be done and make sure the computation is correct

src/LDA.py
@@ -57,7 +57,7 @@ class LDA:
         d[d <= 0] = 0
         d[d > 0] = 1
 
-        return d
+        return np.transpose(d)
 
     # def evaluate_acc(self, ev_x, ev_y):
     #     x = ev_x

src/Main_project.py
@@ -1,17 +1,18 @@
 import numpy as np
 import pandas as pd
+from datetime import datetime
 
 import data_cleaning as dc
 import LDA as lda
 import Pr1 as logR
 
+
 def evaluate_acc(ts_x, ts_y, model):
     x = ts_x
     y = ts_y
     predicted_y = model.predict(x)
-
     err = (np.sum(abs(y - predicted_y)) / len(np.transpose(y)))
-    acc = (1 - err)*100
+    acc = (1 - err)
 
     return err, acc
 
@@ -63,6 +64,8 @@ def model_selection(ev_x, ev_y, model, k=1):
 
 
 def main():
+    start_time = datetime.now()
+
     file_path = '../data/wine/winequality-red.csv'
     raw_data = pd.read_csv(file_path, delimiter=';')
     clean_data = dc.get_clean_data(raw_data)
@@ -78,7 +81,7 @@ def main():
     # Split between X and y and create the numpy arrays
     y_var = 'y'
     x_vars = [var for var in clean_data.columns.tolist() if not var in y_var]
-    X_train, y_train, X_test, y_test = dc.train_test_split(clean_data, x_vars, y_var)
+    X_train, X_test, y_train, y_test = dc.train_test_split(clean_data, x_vars, y_var)
 
     np.seterr(over='ignore')
 
@@ -95,6 +98,7 @@ def main():
     print(ld_avg_err)
     print(ld_avg_acc)
 
+    print("Time lapsed = ", datetime.now() - start_time)
 
 if __name__ == "__main__":
     main()

src/Pr1.py
@@ -68,7 +68,7 @@ class LogisticRegression:
             w = w_init + alpha * np.inner(np.transpose(x), q)
             stop = np.linalg.norm(np.subtract(w, w_init), 2)
             w_init = w
-            i+=1
+            i += 1
 
         self.w_learned = w_init
 