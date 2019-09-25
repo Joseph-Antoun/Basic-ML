Test on the data for wine and fixed errors in logistic regression and LDA more testing should be done and make sure the computation is correct

src/LDA.py
@@ -12,12 +12,11 @@ class LDA:
         self.y = y
         self.n, self.m = np.shape(X)
 
-        self.w_0 = np.ones()
-        self.w_1 = np.ones()
+        self.w_0 = np.ones((self.m, 1))
+        self.w_1 = np.ones((self.m, 1))
 
     def fit(self, X, y):
-        n = self.n
-        m = self.m
+        n, m = np.shape(X)
 
         tr_x = X
         tr_y = y
@@ -28,13 +27,15 @@ class LDA:
         p_0 = n_0/(n_0 + n_1)
         ind_0 = 1 - np.sign(tr_y)
         ind_1 = np.sign(tr_y)
+        ind_0 = ind_0.reshape(n, 1)
+        ind_1 = ind_1.reshape(n, 1)
         muo_0 = np.sum(np.multiply(ind_0, tr_x)/n_0, axis=0)
         muo_0 = muo_0.reshape(1, m)
         muo_1 = np.sum(np.multiply(ind_1, tr_x)/n_1, axis=0)
         muo_1 = muo_1.reshape(1, m)
 
         sigma = np.zeros((m, m))
-        for i in range(0, n+1):
+        for i in range(0, n):
             sigma = sigma + (ind_0[i]*np.matmul(np.transpose(tr_x[i]-muo_0), (tr_x[i] - muo_0)))/(n_0 + n_1 - 2) +\
                     (ind_1[i]*np.matmul(np.transpose(tr_x[i]-muo_1), (tr_x[i] - muo_1)))/(n_0 + n_1 - 2)
 
@@ -51,7 +52,7 @@ class LDA:
 
         d_x = d_x
 
-        d = w_0 + np.matmul(np.transpose(d_x), w_1)
+        d = w_0 + np.matmul(d_x, w_1)
 
         d[d <= 0] = 0
         d[d > 0] = 1

src/Main_project.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 
+import data_cleaning as dc
+import LDA as lda
+import Pr1 as logR
 
 def evaluate_acc(ts_x, ts_y, model):
     x = ts_x
@@ -43,10 +46,11 @@ def model_selection(ev_x, ev_y, model, k=1):
             y_vl = y[i]
             del(temp_x[i])
             del(temp_y[i])
-            x_tr1 = np.concatenate(temp_x)
+            x_tr = np.concatenate(temp_x)
             y_tr = np.concatenate(temp_y)
-            n, m = np.shape(x_tr1)
-            x_tr = np.concatenate(np.ones(n, 1), x_tr1)
+            n, m = np.shape(x_tr)
+            ones = np.ones((n, 1))
+            np.append(ones, x_tr, axis=1)
             model.fit(x_tr, y_tr)
             err, acc = evaluate_acc(x_vl, y_vl, model)
             tot_err.append(err)
@@ -57,6 +61,43 @@ def model_selection(ev_x, ev_y, model, k=1):
 
     return avg_err, avg_acc
 
+
+def main():
+    file_path = '../data/wine/winequality-red.csv'
+    raw_data = pd.read_csv(file_path, delimiter=';')
+    clean_data = dc.get_clean_data(raw_data)
+
+    # this is to skip SettingWithCopyWarning from Pandas
+    clean_df = dc.get_clean_data(raw_data, verbose=False)
+    clean_data = clean_df.copy()
+    # Create the binary y column
+    clean_data['y'] = np.where(clean_df['quality'] >= 6.0, 1.0, 0.0)
+    # Drop the 'quality' column as it shouldn't be used to predict the wine binary rating
+    clean_data.drop('quality', axis=1, inplace=True)
+
+    # Split between X and y and create the numpy arrays
+    y_var = 'y'
+    x_vars = [var for var in clean_data.columns.tolist() if not var in y_var]
+    X_train, y_train, X_test, y_test = dc.train_test_split(clean_data, x_vars, y_var)
+
+    np.seterr(over='ignore')
+
+    lr = logR.LogisticRegression(X_train, y_train, x_vars, y_var)
+
+    lr_avg_err, lr_avg_acc= model_selection(X_train, y_train, lr, 5)
+    print("Logistic Regression")
+    print(lr_avg_err)
+    print(lr_avg_acc)
+
+    ld = lda.LDA(X_train, y_train)
+    ld_avg_err, ld_avg_acc = model_selection(X_train, y_train, ld, 5)
+    print("LDA")
+    print(ld_avg_err)
+    print(ld_avg_acc)
+
+
+if __name__ == "__main__":
+    main()
 
 
 

src/Pr1.py
@@ -1,9 +1,4 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Use the clean_data module (in the same folder) to load the data
-import data_cleaning
 
 
 class LogisticRegression:
@@ -23,7 +18,7 @@ class LogisticRegression:
         self.num_iters = num_iters
         self.alpha = alpha
         self.epsilon = stopping_criteria
-        self.w_learned = np.ones()
+        self.w_learned = np.ones((self.m, 1))
 
     def __repr__(self):
         """
@@ -53,27 +48,27 @@ class LogisticRegression:
         :param t: w^t.x_i
         :return: sigmoid(t)
         """
-
         sig = 1/(1+np.exp(-t))
         return sig
 
-    def fit(self, X, y, n_iters):
+    def fit(self, X, y):
 
         x = X  # this should get the features matrix without the results y
         y = y  # This should get the results of  trained features
         alpha = self.alpha
-        n, m = np.shape(data)
-        w_init = np.random.random(m, 1)
+        n, m = np.shape(X)
+        w_init = np.random.random((m, 1))
         epsilon = self.epsilon
         stop = 1
         i = 0
 
-        while i <= n_iters and stop > epsilon:
+        while i <= self.num_iters and stop > epsilon:
             z = np.matmul(np.transpose(w_init), np.transpose(x))  # calculating the inner input for the sigmoid
             q = np.transpose(y) - self.sigmoid(z)
             w = w_init + alpha * np.inner(np.transpose(x), q)
-            stop = abs(w - w_init)
+            stop = np.linalg.norm(np.subtract(w, w_init), 2)
             w_init = w
+            i+=1
 
         self.w_learned = w_init
 