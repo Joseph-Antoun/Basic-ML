9cdb518f88aa1e6ebbd4503f4ed7f978112fb385
jantoun00 <47575197+jantoun00@users.noreply.github.com>
2019-09-27 19:01:26 -0400

    create main files for breat cancer data in LR and LDA need to tweek alpha and lambda and epsilon to find the optimum solution

---
+++ src/Main_LDA_BC.py
@@ -0,0 +1,127 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+import data_cleaning as dc
+import LDA as lda
+
+
+
+def evaluate_acc(ts_x, ts_y, model):
+    """
+    This method is used to evaluate the accuracy and error of the trained model.
+    :param ts_x: features data to test/ evaluate our model
+    :param ts_y: the real classification of the test data
+    :param model: the model under evaluation
+    :return: acc: accuracy of the model, err: the error of the model
+    """
+    x = ts_x
+    y = ts_y
+    predicted_y = model.predict(x)
+    acc = float(sum(predicted_y == y))/float(len(y))
+    err = 1 - acc
+
+
+    return err, acc
+
+
+def model_selection(ev_x, ev_y, model, k=1):
+    """
+    This method is used for model selection, it applies K-Fold cross validation.
+    :param ev_x: The features data used to train
+    :param ev_y: the classification data used to train
+    :param model: The model that need to apply the k-fold for
+    :param k: the number of folds, if k=1 it will train and validate on the same data
+    :param costs: the list that will hold the cost function results for each fold.
+    :return: return the average error and accuracy of the model with k-fold
+    """
+    tot_err = []
+    tot_acc = []
+    # avg_err = 0.0
+    # avg_acc = 0.0
+
+    if k == 1:
+        n, m = np.shape(ev_x)
+        tr_x = ev_x
+        tr_y = ev_y
+
+        model.fit(tr_x, tr_y)
+        err, acc = evaluate_acc(tr_x, tr_y, model)
+        tot_err.append(err)
+        tot_acc.append(acc)
+
+    elif k > 1:
+        x = np.array_split(ev_x, k)
+        y = np.array_split(ev_y, k)
+
+        for i in range(0, k):
+            temp_x = x.copy()
+            temp_y = y.copy()
+            x_vl = x[i]
+            y_vl = y[i]
+            del(temp_x[i])
+            del(temp_y[i])
+            x_tr = np.concatenate(temp_x)
+            y_tr = np.concatenate(temp_y)
+            model.fit(x_tr, y_tr)
+            err, acc = evaluate_acc(x_vl, y_vl, model)
+            tot_err.append(err)
+            tot_acc.append(acc)
+
+    avg_err = sum(tot_err)/len(tot_err)
+    avg_acc = sum(tot_acc)/len(tot_acc)
+
+    return avg_err, avg_acc
+
+
+def main():
+    start_time = datetime.now()
+
+    file_path = '../data/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
+    header = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size',
+              'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size',
+              'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+    raw_data = pd.read_csv(file_path, names=header)
+    clean_data = dc.get_clean_data_breast_cancer(raw_data, verbose=False)
+    clean_data = dc.compute_y_column_breast_cancer(clean_data)
+
+    # Split between X and y and create the numpy arrays
+    x_vars = clean_data.columns.tolist()
+    y_var = 'y'
+    x_vars.remove(y_var)
+
+    # --------------------------------------------------------------------------
+    # Train test split
+    # --------------------------------------------------------------------------
+    X_train, X_test, y_train, y_test = dc.train_test_split(
+        clean_data,
+        x_vars,
+        y_var)
+
+    np.seterr(over='ignore')
+
+    n_tr, m_tr = np.shape(X_train)
+    n_ts, m_ts = np.shape(X_test)
+
+    y_train = y_train.reshape(n_tr, 1)
+    y_test = y_test.reshape(n_ts, 1)
+
+    ld = lda.LDA(X_train, y_train)
+    ld_avg_err, ld_avg_acc = model_selection(X_train, y_train, ld, 5)
+    print("LDA")
+    print("Average Error with K-fold: ", ld_avg_err)
+    print("Average Accuracy with K-fold: ", ld_avg_acc)
+
+    ts_avg_err, ts_avg_acc = evaluate_acc(X_test, y_test, ld)
+    print("Average Test Error: ", ts_avg_err)
+    print("Average Test Accuracy  : ", ts_avg_acc)
+
+    print("Time lapsed = ", datetime.now() - start_time)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ src/Main_LR_BC.py
@@ -0,0 +1,169 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+import data_cleaning as dc
+import Pr1 as logR
+import LogisticRegression as logR2
+
+
+def evaluate_acc(ts_x, ts_y, model):
+    """
+    This method is used to evaluate the accuracy and error of the trained model.
+    :param ts_x: features data to test/ evaluate our model
+    :param ts_y: the real classification of the test data
+    :param model: the model under evaluation
+    :return: acc: accuracy of the model, err: the error of the model
+    """
+    x = ts_x
+    y = ts_y
+    predicted_y = model.predict(x)
+    acc = float(sum(predicted_y == y))/float(len(y))
+    err = 1 - acc
+
+
+    return err, acc
+
+
+def model_selection(ev_x, ev_y, model, k=1, costs=[]):
+    """
+    This method is used for model selection, it applies K-Fold cross validation.
+    :param ev_x: The features data used to train
+    :param ev_y: the classification data used to train
+    :param model: The model that need to apply the k-fold for
+    :param k: the number of folds, if k=1 it will train and validate on the same data
+    :param costs: the list that will hold the cost function results for each fold.
+    :return: return the average error and accuracy of the model with k-fold
+    """
+    tot_err = []
+    tot_acc = []
+
+    if k == 1:
+        n, m = np.shape(ev_x)
+        tr_x = ev_x
+        tr_y = ev_y
+
+        model.fit(tr_x, tr_y)
+        err, acc = evaluate_acc(tr_x, tr_y, model)
+        tot_err.append(err)
+        tot_acc.append(acc)
+        costs.append(model.h_cost)
+
+    elif k > 1:
+        x = np.array_split(ev_x, k)
+        y = np.array_split(ev_y, k)
+
+        for i in range(0, k):
+            temp_x = x.copy()
+            temp_y = y.copy()
+            x_vl = x[i]
+            y_vl = y[i]
+            del(temp_x[i])
+            del(temp_y[i])
+            x_tr = np.concatenate(temp_x)
+            y_tr = np.concatenate(temp_y)
+            model.fit(x_tr, y_tr)
+            err, acc = evaluate_acc(x_vl, y_vl, model)
+            tot_err.append(err)
+            tot_acc.append(acc)
+            costs.append(model.h_cost)
+
+    avg_err = sum(tot_err)/len(tot_err)
+    avg_acc = sum(tot_acc)/len(tot_acc)
+
+    return avg_err, avg_acc
+
+
+def main():
+    start_time = datetime.now()
+
+    file_path = '../data/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
+    header = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size',
+              'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size',
+              'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+    raw_data = pd.read_csv(file_path, names=header)
+    clean_data = dc.get_clean_data_breast_cancer(raw_data, verbose=False)
+    clean_data = dc.compute_y_column_breast_cancer(clean_data)
+
+    # Split between X and y and create the numpy arrays
+    x_vars = clean_data.columns.tolist()
+    y_var = 'y'
+    x_vars.remove(y_var)
+
+    # --------------------------------------------------------------------------
+    # Train test split
+    # --------------------------------------------------------------------------
+    X_train, X_test, y_train, y_test = dc.train_test_split(
+        clean_data,
+        x_vars,
+        y_var)
+
+    np.seterr(over='ignore')
+
+    X_tr_lr = X_train
+    X_ts_lr = X_test
+
+    n_tr, m_tr = np.shape(X_train)
+    n_ts, m_ts = np.shape(X_test)
+
+    X_tr_lr = np.hstack((np.ones((n_tr, 1)), X_tr_lr))
+    X_ts_lr = np.hstack((np.ones((n_ts, 1)), X_ts_lr))
+
+    y_train = y_train.reshape(n_tr, 1)
+    y_test = y_test.reshape(n_ts, 1)
+
+
+
+    lr = logR.LogisticRegression(X_tr_lr, y_train, x_vars, y_var, alpha=74e-13, num_iters=50,
+                                 stopping_criteria=1e-8)
+    lr2 = logR2.LogisticRegression(X_tr_lr, y_train, x_vars, y_var, alpha=75e-13, reg_val=75e-13, num_iters=100,
+                                   stopping_criteria=1e-20)
+    cost_fcs = []
+    cost_fcs2 = []
+
+    lr_avg_err, lr_avg_acc = model_selection(X_tr_lr, y_train, lr, 5, cost_fcs)
+    print("Logistic Regression")
+    print("Average Error on validation: ", lr_avg_err)
+    print("Average Accuracy on validation: ", lr_avg_acc)
+
+    err, acc = evaluate_acc(X_ts_lr, y_test, lr)
+    print("Average Test Error: ", err)
+    print("Average Test Error: ", acc)
+
+    lr_avg_err2, lr_avg_acc2 = model_selection(X_tr_lr, y_train, lr2, 5, cost_fcs2)
+    print("Logistic Regression2")
+    print("Average Error on validation with regularization: ", lr_avg_err2)
+    print("Average accuracy on validation with regularization: ", lr_avg_acc2)
+
+    err2, acc2 = evaluate_acc(X_ts_lr, y_test, lr2)
+    print("Average Test Error with regularization: ", err2)
+    print("Average Test Error with regularization: ", acc2)
+
+    print("Time lapsed = ", datetime.now() - start_time)
+    plt.figure(1)
+    sns.set_style('white')
+    plt.plot(range(len(cost_fcs[0])), cost_fcs[0])
+    plt.title("Convergence Graph of Cost Function")
+    plt.xlabel("Number of Iterations")
+    plt.ylabel("Cost")
+    plt.figure(2)
+    sns.set_style('white')
+    plt.plot(range(len(cost_fcs2[0])), cost_fcs2[0])
+    plt.title("Convergence Graph of Cost Function2")
+    plt.xlabel("Number of Iterations")
+    plt.ylabel("Cost")
+    plt.show()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
